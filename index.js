c1a3dd42b2fcfc2f7c53cb6d99371b5d86e8265c
in-ceptive <dev@in-ceptive.com>
2022-02-06 16:48:05 +0400

    init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Application } from "./assets/runtime";
+
+useEffect(() => {
+  const { current } = canvasRef;
+  if (current.width) {
+    // const app = new speRuntimeFactory();
+    // app.start(scene, current);
+    new Application().start(scene);
+  }
+}, [canvasRef]);
+
+export default function index() {
+  return (
+    <div>
+      <canvas ref={canvasRef} id="canvas3d"></canvas>
+    </div>
+  );
+}
